299dd512dee5364b6c353ad7d13919b224808443
zhoucheng <zhoucheng133@outlook.com>
2025-01-31 18:55:22 +0800

    完成代码

--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-console.log("Hello via Bun!");
+import Database from 'bun:sqlite';
+import { JSONFilePreset } from 'lowdb/node';
+
+const db = new Database('db/sqlite.db');
+
+// lowdb表的结构，你需要将这里修改为lowdb的结构
+interface List{
+  id: string,
+  title: string,
+}
+// sqlite表的结构，你需要将这里修改为lowdb的结构
+try {
+  db.prepare(`
+    CREATE TABLE list (
+      id TEXT KEY,
+      title TEXT,
+    )
+  `).run();
+} catch (_) {}
+
+const lowdb = await JSONFilePreset<List[]>('db/list.json', []);
+lowdb.read();
+const ls=lowdb.data;
+for(const item of ls){
+  const stmt = db.prepare("INSERT INTO list (id, title) VALUES (?, ?)");
+  stmt.run(item.id, item.title);
+}
